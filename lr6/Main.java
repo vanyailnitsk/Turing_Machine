bc5f9acfd82bd89612f04cf02b9a09af0c09c3c5
vanyailnitsk <vanyailnitsk@yandex.ru>
2022-05-27 21:21:57 +0300

    lr6 mul

--- lr6/Main.java
+++ lr6/Main.java
@@ -3,17 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static int [] sostoyanieArray = new int [30];
     public static int sostoyanie;
-    public static String[] lenta = new String[150];
+    public static String[] lenta = new String[50];
     public static int index=4;
 
     public static void main(String[] args)
     {
-        for (int i=0;i<lenta.length;i++)
-        {
-            lenta[i]="_";
-        }
-        input_binary();
-        index=4;
+        input();
+        index=1;
         sostoyanie=1;
         int check=1;
         int step=1;
@@ -48,156 +44,647 @@ public class Main {
                 case 8:
                     sostoyanie8();
                     break;
+                case 9:
+                    sostoyanie9();
+                    break;
+                case 10:
+                    sostoyanie10();
+                    break;
+                case 11:
+                    sostoyanie11();
+                    break;
+                case 12:
+                    sostoyanie12();
+                    break;
+                case 13:
+                    sostoyanie13();
+                    break;
+                case 14:
+                    sostoyanie14();
+                    break;
+                case 15:
+                    sostoyanie15();
+                    break;
+                case 16:
+                    sostoyanie16();
+                    break;
+                case 17:
+                    sostoyanie17();
+                    break;
                 default:
                     break;
             }
             step++;
         }
-        unary_to_binary();
         System.out.println("\nResult poluchen za "+step+" shagov");
+    }
+    public static void input()
+    {
+        for (int i=0;i<lenta.length;i++)
+        {
+            lenta[i]="_";
+        }
+        lenta[0]="#";
+        for (int i=1;i<9;i++)
+        {
+            lenta[i]="0";
+        }
+        lenta[9]="!";
+        lenta[10]="1";
+        lenta[11]="0";
+        lenta[12]="1";
+        lenta[13]="?";
+        for (int i=14;i<22;i++)
+        {
+            lenta[i]="@";
+        }
+        lenta[22]="!";
+        lenta[23]="1";
+        lenta[24]="0";
+        lenta[25]="1";
+        lenta[26]="1";
+        lenta[27]="#";
+
     }
     public static void sostoyanie1()
     {
-        if (lenta[index]=="1")
+        if (lenta[index]=="0")
         {
-            lenta[index]="_";
-            sostoyanie=2;
+            lenta[index]="0";
+            sostoyanie=1;
             index++;
         }
-        else if (lenta[index]=="_")
+        else if (lenta[index]=="1")
         {
-            lenta[index]="_";
+            lenta[index]="1";
+            sostoyanie=1;
+            index++;
+        }
+        else if (lenta[index]=="#")
+        {
+            lenta[index]="#";
             sostoyanie=8;
+            index--;
+        }
+        else if (lenta[index]=="!")
+        {
+            lenta[index]="!";
+            sostoyanie=1;
+            index++;
+        }
+        else if (lenta[index]=="?")
+        {
+            lenta[index]="?";
+            sostoyanie=1;
+            index++;
+        }
+        else if (lenta[index]=="@")
+        {
+            lenta[index]="@";
+            sostoyanie=1;
+            index++;
+        }
+        else if (lenta[index]=="z")
+        {
+            lenta[index]="z";
+            sostoyanie=1;
+            index++;
+        }
+        else if (lenta[index]=="o")
+        {
+            lenta[index]="o";
+            sostoyanie=1;
             index++;
         }
     }
     public static void sostoyanie2()
     {
-        if (lenta[index]=="1")
+        if (lenta[index]=="0")
+        {
+            lenta[index]="0";
+            sostoyanie=2;
+            index++;
+        }
+        else if (lenta[index]=="1")
         {
             lenta[index]="1";
             sostoyanie=2;
             index++;
         }
+        else if (lenta[index]=="#")
+        {
+            lenta[index]="#";
+            sostoyanie=3;
+            index--;
+        }
+        else if (lenta[index]=="!")
+        {
+            lenta[index]="!";
+            sostoyanie=2;
+            index++;
+        }
+        else if (lenta[index]=="?")
+        {
+            lenta[index]="?";
+            sostoyanie=2;
+            index++;
+        }
+        else if (lenta[index]=="@")
+        {
+            lenta[index]="@";
+            sostoyanie=2;
+            index++;
+        }
+        else if (lenta[index]=="z")
+        {
+            lenta[index]="z";
+            sostoyanie=2;
+            index++;
+        }
+        else if (lenta[index]=="o")
+        {
+            lenta[index]="o";
+            sostoyanie=2;
+            index++;
+        }
+        else if (lenta[index]=="x")
+        {
+            lenta[index]="x";
+            sostoyanie=2;
+            index++;
+        }
         else if (lenta[index]=="_")
         {
-            lenta[index]="_";
-            sostoyanie=3;
-            index++;
+            lenta[index]="#";
+            sostoyanie=2;
+            index--;
         }
     }
     public static void sostoyanie3()
     {
-        if (lenta[index]=="1")
+        if (lenta[index]=="0")
         {
-            lenta[index]="1";
-            sostoyanie=3;
-            index++;
-        }
-        else if (lenta[index]=="*")
-        {
-            lenta[index]="*";
-            sostoyanie=3;
-            index++;
-        }
-        else if (lenta[index]=="_")
-        {
-            lenta[index]="_";
+            lenta[index]="z";
             sostoyanie=4;
+            index--;
+        }
+        else if (lenta[index]=="1")
+        {
+            lenta[index]="o";
+            sostoyanie=5;
+            index--;
+        }
+        else if (lenta[index]=="!")
+        {
+            lenta[index]="!";
+            sostoyanie=6;
+            index--;
+        }
+        else if (lenta[index]=="z")
+        {
+            lenta[index]="z";
+            sostoyanie=3;
+            index--;
+        }
+        else if (lenta[index]=="o")
+        {
+            lenta[index]="o";
+            sostoyanie=3;
             index--;
         }
     }
     public static void sostoyanie4()
     {
-        if (lenta[index]=="1")
+        if (lenta[index]=="0")
         {
-            lenta[index]="*";
-            sostoyanie=5;
-            index++;
-        }
-        else if (lenta[index]=="*")
-        {
-            lenta[index]="*";
+            lenta[index]="0";
             sostoyanie=4;
             index--;
         }
-        else if (lenta[index]=="_")
+        else if (lenta[index]=="1")
         {
-            lenta[index]="_";
-            sostoyanie=7;
+            lenta[index]="1";
+            sostoyanie=4;
             index--;
+        }
+        else if (lenta[index]=="!")
+        {
+            lenta[index]="!";
+            sostoyanie=4;
+            index--;
+        }
+        else if (lenta[index]=="@")
+        {
+            lenta[index]="0";
+            sostoyanie=2;
+            index++;
         }
     }
     public static void sostoyanie5()
     {
-        if (lenta[index]=="1")
+        if (lenta[index]=="0")
         {
-            lenta[index]="1";
+            lenta[index]="0";
             sostoyanie=5;
-            index++;
-        }
-        else if (lenta[index]=="*")
-        {
-            lenta[index]="*";
-            sostoyanie=5;
-            index++;
-        }
-        else if (lenta[index]=="_")
-        {
-            lenta[index]="1";
-            sostoyanie=6;
             index--;
+        }
+        else if (lenta[index]=="1")
+        {
+            lenta[index]="1";
+            sostoyanie=5;
+            index--;
+        }
+        else if (lenta[index]=="!")
+        {
+            lenta[index]="!";
+            sostoyanie=5;
+            index--;
+        }
+        else if (lenta[index]=="@")
+        {
+            lenta[index]="1";
+            sostoyanie=2;
+            index++;
         }
     }
     public static void sostoyanie6()
     {
-        if (lenta[index]=="1")
+        if (lenta[index]=="0")
+        {
+            lenta[index]="0";
+            sostoyanie=6;
+            index--;
+        }
+        else if (lenta[index]=="1")
         {
             lenta[index]="1";
             sostoyanie=6;
             index--;
         }
-        else if (lenta[index]=="*")
+        else if (lenta[index]=="?")
         {
-            lenta[index]="*";
-            sostoyanie=4;
+            lenta[index]="?";
+            sostoyanie=7;
+            index++;
+        }
+        else if (lenta[index]=="@")
+        {
+            lenta[index]="0";
+            sostoyanie=6;
             index--;
         }
     }
     public static void sostoyanie7()
     {
-        if (lenta[index]=="1")
+        if (lenta[index]=="0")
+        {
+            lenta[index]="0";
+            sostoyanie=7;
+            index++;
+        }
+        else if (lenta[index]=="1")
         {
             lenta[index]="1";
             sostoyanie=7;
+            index++;
+        }
+        else if (lenta[index]=="#")
+        {
+            lenta[index]="#";
+            sostoyanie=10;
             index--;
         }
-        else if (lenta[index]=="_")
+        else if (lenta[index]=="!")
         {
-            lenta[index]="_";
-            sostoyanie=1;
+            lenta[index]="!";
+            sostoyanie=7;
+            index++;
+        }
+        else if (lenta[index]=="z")
+        {
+            lenta[index]="0";
+            sostoyanie=7;
+            index++;
+        }
+        else if (lenta[index]=="o")
+        {
+            lenta[index]="1";
+            sostoyanie=7;
             index++;
         }
     }
     public static void sostoyanie8()
     {
-        if (lenta[index]=="1")
+        if (lenta[index]=="0")
         {
-            lenta[index]="_";
+            lenta[index]="0";
             sostoyanie=8;
-            index++;
+            index--;
         }
-        else if (lenta[index]=="*")
+        else if (lenta[index]=="1")
         {
             lenta[index]="1";
             sostoyanie=8;
+            index--;
+        }
+        else if (lenta[index]=="!")
+        {
+            lenta[index]="!";
+            sostoyanie=8;
+            index--;
+        }
+        else if (lenta[index]=="?")
+        {
+            lenta[index]="?";
+            sostoyanie=9;
+            index--;
+        }
+        else if (lenta[index]=="@")
+        {
+            lenta[index]="@";
+            sostoyanie=8;
+            index--;
+        }
+    }
+    public static void sostoyanie9()
+    {
+        if (lenta[index]=="0")
+        {
+            lenta[index]="x";
+            sostoyanie=17;
+            index++;
+        }
+        else if (lenta[index]=="1")
+        {
+            lenta[index]="x";
+            sostoyanie=2;
+            index++;
+        }
+        else if (lenta[index]=="!")
+        {
+            lenta[index]="!";
+            sostoyanie=0;
+        }
+        else if (lenta[index]=="x")
+        {
+            lenta[index]="x";
+            sostoyanie=9;
+            index--;
+        }
+    }
+    public static void sostoyanie10()
+    {
+        if (lenta[index]=="0")
+        {
+            lenta[index]="0";
+            sostoyanie=10;
+            index--;
+        }
+        else if (lenta[index]=="1")
+        {
+            lenta[index]="1";
+            sostoyanie=10;
+            index--;
+        }
+        else if (lenta[index]=="!")
+        {
+            lenta[index]="!";
+            sostoyanie=11;
+            index--;
+        }
+        else if (lenta[index]=="?")
+        {
+            lenta[index]="?";
+            sostoyanie=10;
+            index--;
+        }
+        else if (lenta[index]=="x")
+        {
+            lenta[index]="x";
+            sostoyanie=10;
+            index--;
+        }
+    }
+    public static void sostoyanie11()
+    {
+        if (lenta[index]=="0")
+        {
+            lenta[index]="1";
+            sostoyanie=11;
+            index--;
+        }
+        else if (lenta[index]=="1")
+        {
+            lenta[index]="0";
+            sostoyanie=12;
+            index--;
+        }
+        else if (lenta[index]=="?")
+        {
+            lenta[index]="?";
+            sostoyanie=16;
+            index++;
+        }
+    }
+    public static void sostoyanie12()
+    {
+        if (lenta[index]=="0")
+        {
+            lenta[index]="0";
+            sostoyanie=12;
+            index--;
+        }
+        else if (lenta[index]=="1")
+        {
+            lenta[index]="1";
+            sostoyanie=12;
+            index--;
+        }
+        else if (lenta[index]=="!")
+        {
+            lenta[index]="!";
+            sostoyanie=13;
+            index--;
+        }
+        else if (lenta[index]=="?")
+        {
+            lenta[index]="?";
+            sostoyanie=12;
+            index--;
+        }
+        else if (lenta[index]=="x")
+        {
+            lenta[index]="x";
+            sostoyanie=12;
+            index--;
+        }
+    }
+    public static void sostoyanie13()
+    {
+        if (lenta[index]=="0")
+        {
+            lenta[index]="1";
+            sostoyanie=14;
+            index++;
+        }
+        else if (lenta[index]=="1")
+        {
+            lenta[index]="0";
+            sostoyanie=13;
+            index--;
+        }
+        else if (lenta[index]=="#")
+        {
+            lenta[index]="#";
+            sostoyanie=14;
+            index++;
+        }
+        else if (lenta[index]=="@")
+        {
+            lenta[index]="1";
+            sostoyanie=14;
+            index++;
+        }
+    }
+    public static void sostoyanie14()
+    {
+        if (lenta[index]=="0")
+        {
+            lenta[index]="0";
+            sostoyanie=14;
+            index++;
+        }
+        else if (lenta[index]=="1")
+        {
+            lenta[index]="1";
+            sostoyanie=14;
+            index++;
+        }
+        else if (lenta[index]=="!")
+        {
+            lenta[index]="!";
+            sostoyanie=14;
+            index++;
+        }
+        else if (lenta[index]=="?")
+        {
+            lenta[index]="?";
+            sostoyanie=15;
+            index++;
+        }
+        else if (lenta[index]=="x")
+        {
+            lenta[index]="x";
+            sostoyanie=14;
+            index++;
+        }
+    }
+    public static void sostoyanie15()
+    {
+        if (lenta[index]=="0")
+        {
+            lenta[index]="0";
+            sostoyanie=15;
+            index++;
+        }
+        else if (lenta[index]=="1")
+        {
+            lenta[index]="1";
+            sostoyanie=15;
+            index++;
+        }
+        else if (lenta[index]=="!")
+        {
+            lenta[index]="!";
+            sostoyanie=11;
+            index--;
+        }
+        else if (lenta[index]=="@")
+        {
+            lenta[index]="@";
+            sostoyanie=15;
+            index++;
+        }
+    }
+    public static void sostoyanie16()
+    {
+        if (lenta[index]=="0")
+        {
+            lenta[index]="@";
+            sostoyanie=16;
+            index++;
+        }
+        else if (lenta[index]=="1")
+        {
+            lenta[index]="@";
+            sostoyanie=16;
+            index++;
+        }
+        else if (lenta[index]=="!")
+        {
+            lenta[index]="!";
+            sostoyanie=17;
+            index++;
+        }
+        else if (lenta[index]=="@")
+        {
+            lenta[index]="@";
+            sostoyanie=16;
+            index++;
+        }
+    }
+    public static void sostoyanie17()
+    {
+        if (lenta[index]=="0")
+        {
+            lenta[index]="0";
+            sostoyanie=17;
+            index++;
+        }
+        else if (lenta[index]=="1")
+        {
+            lenta[index]="1";
+            sostoyanie=17;
+            index++;
+        }
+        else if (lenta[index]=="#")
+        {
+            lenta[index]="0";
+            sostoyanie=8;
+            index--;
+        }
+        else if (lenta[index]=="!")
+        {
+            lenta[index]="!";
+            sostoyanie=17;
+            index++;
+        }
+        else if (lenta[index]=="?")
+        {
+            lenta[index]="?";
+            sostoyanie=17;
+            index++;
+        }
+        else if (lenta[index]=="@")
+        {
+            lenta[index]="@";
+            sostoyanie=17;
+            index++;
+        }
+        else if (lenta[index]=="x")
+        {
+            lenta[index]="x";
+            sostoyanie=17;
             index++;
         }
         else if (lenta[index]=="_")
         {
-            sostoyanie=0;
+            lenta[index]="0";
+            sostoyanie=8;
+            index--;
         }
     }
+
     public static void print_LENTA()
     {
         System.out.print("Lenta:     ");
@@ -222,39 +709,8 @@ public class Main {
         }
         System.out.println("\n");
     }
-    public static void input_binary()
-    {
-        Scanner in = new Scanner(System.in);
-        int num1 = in.nextInt();
-        num1 = Integer.parseInt(Integer.toString(num1),2);
-        for (int i=0;i<num1;i++)
-        {
-            lenta[index]="1";
-            index++;
-        }
-        index++;
-        int num2 = in.nextInt();
-        num2 = Integer.parseInt(Integer.toString(num2),2);
-        for (int i=0;i<num2;i++)
-        {
-            lenta[index]="1";
-            index++;
-        }
 
-    }
-    public static void unary_to_binary()
-    {
-        int result=0;
-        for (int i=0;i<lenta.length;i++)
-        {
-            if (lenta[i]=="1")
-            {
-                result++;
-            }
-        }
-        System.out.println("Result in decimal: "+result);
-        System.out.println("Result in binary:  "+Integer.toBinaryString(result));
-    }
+
 
 }
 
